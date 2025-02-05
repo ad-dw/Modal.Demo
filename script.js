a302096cb9398cc80d5ac11bbfe814e2c57878be
ad-dw <aditya_dwivedi01@outlook.com>
2025-02-05 19:45:21 +0530

    Added show modal functionality.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+const openModal = function () {
+  document.querySelector(".modal").classList.remove("hidden");
+};
+const modalTriggerButtons = document.querySelectorAll(".show-modal");
+modalTriggerButtons.forEach((button) =>
+  button.addEventListener("click", openModal)
+);
