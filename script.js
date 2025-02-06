f7ccec78ef1b20947a323248a67351aae54a189c
ad-dw <aditya_dwivedi01@outlook.com>
2025-02-06 14:01:59 +0530

    Added logic to close modal.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 "use strict";
 
-const openModal = function () {
-  document.querySelector(".modal").classList.remove("hidden");
+const modalElement = document.querySelector(".modal");
+const overlayElement = document.querySelector(".overlay");
+const closeModalButton = document.querySelector(".close-modal");
+
+const closeModal = function () {
+  modalElement.classList.add("hidden");
+  overlayElement.classList.add("hidden");
 };
+
+const openModal = function () {
+  modalElement.classList.remove("hidden");
+  overlayElement.classList.remove("hidden");
+};
+
 const modalTriggerButtons = document.querySelectorAll(".show-modal");
 modalTriggerButtons.forEach((button) =>
   button.addEventListener("click", openModal)
 );
+
+closeModalButton.addEventListener("click", closeModal);
